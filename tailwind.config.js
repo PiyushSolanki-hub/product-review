7b8881f2762d0293b9997b1c22f9e8d25c4983d8
Piyush Solanki <piyushsolanki2521@gmail.com>
2025-01-05 14:41:44 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'fraunces': ['Fraunces', 'serif'],
+        'montserrat': ['Montserrat', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
